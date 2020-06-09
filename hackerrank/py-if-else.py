0ed5b2d44039b10ad2021bedca0605281f8e0b11
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2020-06-09 16:10:43 -0300

    py-if-else

---
+++ hackerrank/py-if-else.py
@@ -0,0 +1,12 @@
+"""
+https://www.hackerrank.com/contests/linguagens-de-programacao-para-ciencia-de-dados/challenges/py-if-else
+"""
+n = int(input("Digite um número inteiro:\n"))
+if 2 <= n <= 5 and n % 2 == 0:
+    print("Not Weird")
+elif 6 <= n <= 20 and n % 2 == 0:
+    print("Weird")
+elif 20 > n % 2 == 0:
+    print("Not Weird")
+else:
+    print("Weird")
