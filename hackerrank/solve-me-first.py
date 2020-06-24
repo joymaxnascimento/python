9c9384e499310adca5059dc073f66e1d256055e3
Joymax Nascimento <joymaxnascimento@yahoo.com.br>
2020-06-24 13:55:26 -0300

    23 Exercícios

---
+++ hackerrank/solve-me-first.py
@@ -0,0 +1,13 @@
+"""
+https://www.hackerrank.com/contests/linguagens-de-programacao-para-ciencia-de-dados/challenges/solve-me-first
+"""
+
+
+def solveMeFirst(a, b):
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)
