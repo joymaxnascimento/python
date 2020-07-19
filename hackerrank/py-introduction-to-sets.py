2aae48fd25dd39eca27a51654d9f7e57db37bf7c
Joymax <joymaxnascimento@users.noreply.github.com>
2020-07-19 19:20:52 -0300

    31 exercícios resolvidos

---
+++ hackerrank/py-introduction-to-sets.py
@@ -0,0 +1,7 @@
+"""
+https://www.hackerrank.com/contests/linguagens-de-programacao-para-ciencia-de-dados/challenges/py-introduction-to-sets/
+"""
+
+def average(array):
+    x = set(array)
+    return sum(x)/len(x)
